825e52a900f675d0eff9fa8253d52783745ae073
Benjamin Gutierrez <68666564+BenjaGood@users.noreply.github.com>
2023-04-13 17:55:31 -0600

    agrego archivos

---
+++ main.js
@@ -0,0 +1,12 @@
+// JavaScript
+function changeColor(newColor) {
+    var elem = document.getElementById('the-title');
+    elem.style.color = newColor;
+ }
+ 
+ function changeNewButton() {
+    var newButton = document.getElementById('new-button');
+    newButton.style.backgroundColor = 'purple';
+    newButton.style.color = 'white';
+ }
+ 
